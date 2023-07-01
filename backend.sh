f9d70948e86e31a188a55124652920644292090b
sanjeevk021995 <122021144+sanjeevk021995@users.noreply.github.com>
2023-07-01 05:33:50 -0700

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "sample-dev-env-state"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
  //   role_arn     = var.AWS_ROLE_ARN
   }
 }
