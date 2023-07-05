0c79d1012b34fd2c95fd54ec92d9edc46e523a5e
sanjeevk021995 <122021144+sanjeevk021995@users.noreply.github.com>
2023-07-05 19:44:20 +0530

    Update backend.tf

--- roles/backend.tf
+++ roles/backend.tf
@@ -1,16 +1,5 @@
-terraform {
-
-  provider "aws" {
-  region     = "us-east-2"
-  assume_role {
-  role_arn     = var.AWS_ROLE_ARN
-  
-  }
-  
-}
-  backend "s3" {
+backend "s3" {
     bucket = "sample-dev-env-state"
     key    = "roles.tfstate"
     region = "us-east-2"
   }
-}
