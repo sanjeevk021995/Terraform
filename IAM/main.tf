cc3ae8335475e3f8f518984ab1be550d1a39e302
sanjeevk021995 <122021144+sanjeevk021995@users.noreply.github.com>
2023-07-14 09:22:47 +0530

    Create main.tf

---
+++ IAM/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "us-east-2"
+assume_role {
+    role_arn     = var.AWS_ROLE_ARN
+  
+  }
+  
+}
+
+variable "AWS_ROLE_ARN" {
+  description = "aws arn"
+  type        = string
+  sensitive   = true
+}
+
+
+resource "aws_s3_bucket" "testbucket-sam-test-IAM" {
+  bucket = "sample-dev-iambucket"
+  tags = {
+    Name         = "S3 Bucket Created by TS"
+    Environment  = "iam-role"
+    SecurityZone = "na"
+  }
+}
